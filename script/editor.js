94db731b2831431cb87054f32197db234d60d7fb
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-10-31 17:53:40 +0200

    nice progress

--- script/editor.js
+++ script/editor.js
@@ -3,7 +3,7 @@ const bitmapCtx = bitmap.getContext("2d");
 const settingsBtn = document.querySelector("#settings");
 const printBtn = document.querySelector("#print");
 tooltip.create(settingsBtn, "Settings menu");
-tooltip.create(printBtn, "Print image to console");
+tooltip.create(printBtn, "Print image to console as python");
 
 class BitmapEditor {
 	constructor() {
@@ -13,6 +13,7 @@ class BitmapEditor {
 		this.autoZoom();
 		this.prevHover = { x: -1, y: -1 };
 		this.drawing = { down: false, color: -1 };
+		this.name = "drawing1";
 
 		this.generateMap();
 	}
@@ -87,15 +88,72 @@ class BitmapEditor {
 		}
 	}
 
-	generateMap() {
+	generateMap(createMap = true) {
 		const size = this.getSize();
 		bitmap.width = settings.width * size;
 		bitmap.height = settings.height * size;
-		this.map = new Array(settings.height).fill(0).map((x) => new Array(settings.width).fill(0));
+		this.prevHover = { x: -1, y: -1 };
+		this.drawing = { down: false, color: -1 };
+		if (createMap) {
+			this.map = new Array(settings.height).fill(1).map((x) => new Array(settings.width).fill(1));
+		}
 		for (let y = 0; y < this.map.length; y++) {
 			for (let x = 0; x < this.map[y].length; x++) {
 				const col = this.map[y][x];
 				this.drawPixel(x, y, col);
+			}
+		}
+	}
+
+	// This function updates the current map to match new width and height
+	// If "new" is selected when applying settings, the map is completely wiped.
+	// Otherwise, the map gets padded on x and y coords to match.
+	// This padding can't center the original image.
+	update() {
+		this.autoZoom();
+		// Check if the map's size has changed
+		if (this.map.length !== settings.height || this.map[0].length !== settings.width) {
+			if (settings.new) {
+				this.generateMap(); // just create new empty map
+			} else {
+				// Keeping old image, more precise maneuvers needed
+
+				// Find out how much the size has changed
+				const differenceX = settings.width - this.map[0].length;
+				const differenceY = settings.height - this.map.length;
+				// Y-axis is higher than before
+				if (differenceY > 0) {
+					for (let i = 0; i < differenceY; i++) {
+						this.map.push(new Array(settings.width).fill(0));
+					}
+					// Y-axis is lower than before
+				} else if (differenceY < 0) {
+					const startLen = this.map.length + differenceY;
+					for (let i = this.map.length - 1; i >= startLen; i--) {
+						this.map.splice(i, 1);
+					}
+				}
+				// X-axis is higher than before
+				if (differenceX > 0) {
+					for (let y = 0; y < this.map.length; y++) {
+						if (this.map[y].length !== settings.width) {
+							for (let x = 0; x < differenceX; x++) {
+								this.map[y].push(1);
+							}
+						}
+					}
+					// X-axis is lower than before
+				} else if (differenceX < 0) {
+					const startLen = this.map[0].length + differenceX;
+					for (let y = 0; y < this.map.length; y++) {
+						if (this.map[y].length !== settings.width) {
+							for (let i = this.map[y].length - 1; i >= startLen; i--) {
+								this.map[y].splice(i, 1);
+							}
+						}
+					}
+				}
+				this.generateMap(false);
 			}
 		}
 	}
@@ -134,7 +192,10 @@ class BitmapEditor {
 				hexMap.push(`0x${byte.toString(16).padStart(2, "0")}`);
 			}
 		}
-		console.log(`[${hexMap.toString()}]`);
+		console.log(`
+		${this.name}_base = bytearray([${hexMap.toString()}])
+		${this.name} = framebuf.FrameBuffer(${this.name}_base, ${settings.width}, ${settings.height}, framebuf.MONO_VLSB)
+		`);
 	}
 }
 
--- script/settings.js
+++ script/settings.js
@@ -2,13 +2,18 @@ const settingsElem = document.querySelector(".settings");
 
 class Settings {
 	constructor() {
-		this.width = 13;
-		this.height = 10;
+		this.width = 16;
+		this.height = 12;
+		this.new = false;
+		this.isOpen = false;
 		this.unsaved = {};
 	}
 
+	// Resets and opens the settings menu
 	open() {
+		if (this.isOpen) return this.close();
 		settingsElem.textContent = "";
+		this.isOpen = true;
 		this.unsaved = {};
 		const options = [
 			{
@@ -32,7 +37,7 @@ class Settings {
 			},
 		];
 		options.forEach((opt) => {
-			this.unsaved[opt.id] = this.default ?? null;
+			this.unsaved[opt.id] = this.default ?? this[opt.id];
 			const setting = document.createElement("div");
 			setting.classList.add("setting");
 
@@ -47,10 +52,51 @@ class Settings {
 
 			input.value = opt.default ?? this[opt.id];
 
+			input.addEventListener("input", () => {
+				if (opt.type === "number") {
+					this.unsaved[opt.id] = parseInt(input.value);
+				} else {
+					this.unsaved[opt.id] = input.value;
+				}
+			});
+
 			tooltip.create(setting, opt.tt);
 			setting.append(input, label);
 			settingsElem.append(setting);
 		});
+		const saveButton = document.createElement("button");
+		saveButton.textContent = "Apply";
+		saveButton.classList.add("button-basic");
+		saveButton.addEventListener("click", () => {
+			this.apply();
+		});
+		tooltip.create(saveButton, "Apply current settings");
+		settingsElem.append(saveButton);
+		const cancelButton = document.createElement("button");
+		cancelButton.textContent = "Close";
+		cancelButton.classList.add("button-basic");
+		cancelButton.addEventListener("click", () => {
+			this.close();
+		});
+		tooltip.create(cancelButton, "Close settings menu");
+		settingsElem.append(cancelButton);
+	}
+
+	close() {
+		this.isOpen = false;
+		settingsElem.textContent = "";
+		tooltip.hide();
+	}
+
+	// Applies unsaved changes to the current settings object
+	// and then updates the bitmap
+	apply() {
+		Object.entries(this.unsaved).forEach(([key, value]) => {
+			this[key] = value;
+		});
+		this.close();
+		bitmapEditor.update();
+		this.new = false;
 	}
 }
 
